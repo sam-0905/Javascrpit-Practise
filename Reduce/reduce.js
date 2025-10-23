cce35b26ac2cffdc5fe66e15021a4b711d28fcf3
Sam <118877482+sam-0905@users.noreply.github.com>
2025-10-23 20:15:41 +0530

    updated

--- Reduce/reduce.js
+++ Reduce/reduce.js
@@ -1,3 +1,30 @@
+/*
+Array.reduce()- It accepts a callback function and initial value as an input
+(initial value is optional). The function will be called for each element
+of the array with the initial value at the beginning and then with the
+value returned from the last call of the same function.
+
+The callback function has 4 parameters, ( previousValue , currentValue ,
+currentIndex , array ).
+● previousValue – The value returned from the last call of the
+same function or the initial value at the beginning.
+● currentValue – Current value of the array.
+● currentIndex – Current index position of the iteration.
+● array – The array itself.
+
+Using this method we can perform different types of operations.
+
+Gemini vr => To execute a reducer function (that you provide) on each element of the array, resulting in a single output value. It iterates through the array and accumulates a single result.
+---------
+
+2.What is the optional second argument passed to reduce() and why is it important?
+
+Expected Answer: The optional second argument is the initial value for the accumulator. It's important because:
+
+If provided, the accumulation starts with this value and the first element is the currentValue.
+
+If not provided, the first element of the array is used as the initial accumulator value, and iteration starts from the second element.
+ */
 
 // Fin sum
 const arr = [4,5,13,8,19,20];
@@ -72,3 +99,16 @@ const output5 = users.reduce((acc,cur) => {
 },[])
 
 console.log(output5)
+
+
+// How can you use reduce() to flatten an array of arrays (e.g., [[1, 2], [3, 4]] to [1, 2, 3, 4])?
+
+const nested = [[1, 2], [3, 4]];
+
+//Method 1: concat
+const flat = nested.reduce((acc,cur) => acc.concat(cur) , []);
+console.log(flat);
+
+const flat2 = nested.reduce((acc,cur) => [...acc,...cur],[])
+console.log(flat2)
+
