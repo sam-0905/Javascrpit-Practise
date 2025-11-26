98512d98fb2cd714fb376093d7d526e2015743ae
Sam <118877482+sam-0905@users.noreply.github.com>
2025-11-26 12:18:17 +0530

    updated

--- Debounce/debounce.js
+++ Debounce/debounce.js
@@ -1,4 +1,15 @@
-function getData(){
 let counter = 0;
-console.log("Fetch Data ... ", counter)
+
+function getData(){
+console.log("Fetch Data ... ",counter++)
 }
+
+function doSomeMagic(fn, d){
+return function(){
+    setTimeout(()=>{
+    getData()
+    },d)
+}
+}
+
+const fakeDelay = doSomeMagic(getData,300);
