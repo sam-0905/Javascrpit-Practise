4f9bb35c8d77585d76610688b7f39ba97d6b3cd2
Sam <118877482+sam-0905@users.noreply.github.com>
2026-01-20 22:26:14 +0530

    updated

--- DSA/dsa.jsx
+++ DSA/dsa.jsx
@@ -188,9 +188,14 @@ function reverse(str){
 // Question - 4
 // Palindromes
 // Given a string, return true if the string is a palindrome
-// or false if it is not.  Palindromes are strings that
-// form the same word if it is reversed. *Do* include spaces
-// and punctuation in determining if the string is a palindrome.
+// or false if it is not. 
+
+/*
+Palindromes are strings that
+form the same word if it is reversed. *Do* include spaces
+and punctuation in determining if the string is a palindrome
+*/  
+
 // --- Examples:
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
