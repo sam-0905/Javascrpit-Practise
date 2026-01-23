c03a35c59fc1c2acdf6c7503ebf5c3c038d86354
Sam <118877482+sam-0905@users.noreply.github.com>
2026-01-23 23:17:57 +0530

    Question 6 updated

--- DSA/dsa.jsx
+++ DSA/dsa.jsx
@@ -293,3 +293,13 @@ function reverseInt3(num){
 
 
 
+// Question - 6
+// MaxChar
+
+/*
+Given a string, return the character that is most
+commonly used in the string.
+--- Examples
+maxChar("abcccccccd") === "c"
+maxChar("apple 1231111") === "1"
+**/ 
