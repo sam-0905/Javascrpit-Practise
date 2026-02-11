c0ef3f67ec508a8b8dc200518425791ca51e8285
Sam <118877482+sam-0905@users.noreply.github.com>
2026-02-11 13:46:36 +0530

    Q-6 1.a)MaxChar solution updated

--- DSA/dsa.jsx
+++ DSA/dsa.jsx
@@ -326,3 +326,24 @@ for(let char of string){
 }
 
  
+/* MaxChar solution
+  The Goal: Find the character that appears most frequently.
+*/ 
+
+function maxChar(str){
+  const charMap = {};
+  let max = 0;
+  let maxChar ="";
+
+  for(let char of str){
+    charMap[char] = charMap[char] + 1 || 1
+  }
+
+  for(let char in charMap){
+    if(charMap[char] > max){
+      max = charMap[char]
+      maxChar = char
+    }
+  }
+ return maxChar;
+}
