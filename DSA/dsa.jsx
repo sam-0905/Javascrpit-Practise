90d908ccacbce8881da5313338714243aed5d09f
Sam <118877482+sam-0905@users.noreply.github.com>
2026-03-02 08:51:55 +0530

    solution 1 updated for Q-9

--- DSA/dsa.jsx
+++ DSA/dsa.jsx
@@ -565,3 +565,15 @@ return newSentence;
 }
 
 capitalize('a short sentence')
+
+
+// solution 1
+
+function capitalize1(str){
+  const words = [];
+
+  for(let word of str.split(" ")){
+    words.push(word[0].toUpperCase() + word.slice(1))
+  }
+  return words.join(" ")
+} 
