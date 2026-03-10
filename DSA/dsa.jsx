6e05cc8222909d714f772a9c65c6c49bc4678218
Sam <118877482+sam-0905@users.noreply.github.com>
2026-03-10 19:16:37 +0530

    Solution 1 updated  for question - 10

--- DSA/dsa.jsx
+++ DSA/dsa.jsx
@@ -577,3 +577,48 @@ function capitalize1(str){
   }
   return words.join(" ")
 } 
+
+// Question - 10
+
+// steps
+
+/*
+--- Directions
+Write a function that accepts a positive number N.
+The function should console log a step shape
+with N levels using the # character.  Make sure the
+step has spaces on the right hand side!
+--- Examples
+  steps(2)
+      '# '
+      '##'
+  steps(3)
+      '#  '
+      '## '
+      '###'
+  steps(4)
+      '#   '
+      '##  '
+      '### '
+      '####'
+*
+ **/ 
+
+
+// Solution 1
+
+function steps(n){
+
+  for(let row=0; row<n; row++){
+    let stair = ""
+
+  for(let column = 0 ; column<n; column++){
+    if(column <= row){
+      stair += "#"
+    }else{
+      stair += " "
+    }
+  }
+  console.log(stair);
+  }
+}
