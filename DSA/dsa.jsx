8631d9c3a66683b96529a2a87b55c9d8367cc7ae
Sam <118877482+sam-0905@users.noreply.github.com>
2026-03-15 19:19:30 +0530

    Q11 - solution1 updated

--- DSA/dsa.jsx
+++ DSA/dsa.jsx
@@ -649,3 +649,22 @@ pyramid has spaces on both the left *and* right hand sides
 */ 
 
 
+function pyramid(n){
+
+const midpoint = Math.floor((2* n- 1) / 2)
+
+for(let row = 0; row < n; row ++){
+  let level = ""
+
+for(let column = 0; column < 2 * n - 1; column++){
+  if(midpoint - row <=column && midpoint + row >= column){
+    level += "#"
+  }
+  else{
+    level += " "
+  }
+}
+console.log(level)
+}
+
+}
