3002df75b49fe28bf1fe82badaea6867c276a830
Sam <118877482+sam-0905@users.noreply.github.com>
2026-03-15 19:02:52 +0530

    created a  new  Q11- Pyramid

--- DSA/dsa.jsx
+++ DSA/dsa.jsx
@@ -622,3 +622,30 @@ function steps(n){
   console.log(stair);
   }
 }
+
+
+
+// Question - 11
+
+// Pyramid
+
+
+/**
+--- Directions
+Write a function that accepts a positive number N.
+The function should console log a pyramid shape
+with N levels using the # character.  Make sure the
+pyramid has spaces on both the left *and* right hand sides
+--- Examples
+  pyramid(1)
+      '#'
+  pyramid(2)
+      ' # '
+      '###'
+  pyramid(3)
+      '  #  '
+      ' ### '
+      '#####' 
+*/ 
+
+
