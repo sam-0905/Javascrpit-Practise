289c5e8d52eccc828511ef62f3b5e836e82ec29e
Sam <118877482+sam-0905@users.noreply.github.com>
2026-03-17 12:46:54 +0530

    Q-11 Added recursive solution

--- DSA/dsa.jsx
+++ DSA/dsa.jsx
@@ -693,3 +693,31 @@ console.log(level)
 }
 
 }
+
+// Recrusive solution 
+
+function recPyramid(n, row =0, level = "" ){
+
+if(n === row){
+  return;
+}
+
+if(level.length === 2*n-1){
+  console.log(level)
+  return recPyramid(n,row + 1)
+}
+
+const midPoint = Math.floor((2 * n - 1) / 2)
+let add;
+
+if(midPoint -row <= level.length && midPoint + row >= level.length){
+  add = "#"
+}else{
+  add = " "
+}
+
+ recPyramid(n,row,level+add)
+
+}
+
+recPyramid(3)
