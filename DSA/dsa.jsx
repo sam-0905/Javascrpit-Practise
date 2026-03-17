192f82e9f5771c6fd18726d5a4284f6beed9ac6e
Sam <118877482+sam-0905@users.noreply.github.com>
2026-03-17 13:25:32 +0530

    Added a new question  -12 Vowels

--- DSA/dsa.jsx
+++ DSA/dsa.jsx
@@ -721,3 +721,20 @@ if(midPoint -row <= level.length && midPoint + row >= level.length){
 }
 
 recPyramid(3)
+
+
+
+// Question - 11
+
+// Vowels
+
+/*
+--- Directions
+Write a function that returns the number of vowels
+used in a string.  Vowels are the characters 'a', 'e'
+'i', 'o', and 'u'.
+--- Examples
+  vowels('Hi There!') --> 3
+  vowels('Why do you ask?') --> 4
+  vowels('Why?') --> 0
+*/ 
