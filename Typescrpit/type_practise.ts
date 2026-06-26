d389fd4fec53b07d418f4837bbc6f8393a8523df
Allan sam <allansam0905@gmail.com>
2026-06-26 20:31:14 +0530

    feat - Added a new folder for Typescrpit

---
+++ Typescrpit/type_practise.ts
@@ -0,0 +1,130 @@
+// Primitives
+var a : number = 10;
+let username : string = "Allan"
+let isAdmin : boolean = true
+
+// Array
+
+let num: number[] = [1,2,3,4,]
+let fruits : String[] = ["Apple","orange","Grapes","Mango"] 
+
+// Tuples - suppose we need to add both the types means
+
+let data : [number,string] = [2,"Indian Bank"]
+
+// Multiple numbers and multiple strings in a fixed order
+let data1: [number, number, string, string] = [10, 20, "X", "Y"];
+
+// REST - A single string followed by multiple numbers 
+let values: [string, ...number[]] = ["Coordinates", 1, 2, 3, 4, 5];
+
+// ------------------------------------------------------------------------
+
+// enum 
+
+enum Color{
+    Red,
+    Green,
+    Blue
+}
+// It will only take any of these colors inside of it
+let favColor : Color = Color.Blue
+
+
+// Any (Avoid when possible)
+
+let radomValue : any = 10;
+radomValue = "Allan"
+radomValue = true
+
+
+// Unknown (Safer than any)
+let userInput : unknown;
+userInput = 5;
+userInput = "text";
+
+
+// Void (For functions that don't return)
+function subscribe(message : string): void{
+ console.log(message)
+//   return "Hello There!" *It will throw Error!!*
+}
+
+function subscribe2(message : string): string{
+ console.log(message)
+ return "Hello There!"
+}
+
+// Null and undefined
+
+let nullVal: null = null;
+let undefinedVal : undefined = undefined;
+
+
+// --------------------------------------------------------------------------
+// function
+
+
+//Basic function
+
+function add(a:number,b:number) : number{
+return a + b
+}
+
+
+// Optional Parameters
+function greet(name: string, greetings :string):string{
+    return `Hello ${name}`
+}
+
+function greet2(name: string, greetings?:string):string{
+    if(greetings){
+        return `${greetings} ,${name} ! `
+    }
+    return `Hello ${name}`
+}
+
+// Default parameters
+function multiply(a: number, b: number = 1): number{
+    return a * b;
+} 
+
+// REST parameters
+function sum(...number :number[]) :number{
+    return number.reduce((total,n) => total + n , 0)
+}
+
+// Arrow function 
+
+const divide = (a:number , b: number):number => a/b
+
+// Function types
+let calculate : (x:number,y:number) => number;
+calculate = add;   // It will take predefined function
+
+//-------------------------------------------------------------------
+
+// Object interface 
+
+// Object type annotation
+
+let user : {name : string; age : number} = {
+    name : "allan",
+    age : 25,
+}
+
+// Interface
+interface user1{
+    name: string;
+    age : number;
+    email?: string;   // Optional entry
+
+}
+
+// Object type annotation
+
+let user1 : {name : string; age : number} = {
+    name : "allan",
+    age : 25,
+    email : "abcd@gmail.com"
+}
