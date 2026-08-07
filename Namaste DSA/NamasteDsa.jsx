8d5e0316a429872455bf7769c49e7a9c961f47cb
Allan sam <allansam0905@gmail.com>
2026-08-07 12:00:11 +0530

    P1- Find the index using for loop with an array

--- Namaste DSA/NamasteDsa.jsx
+++ Namaste DSA/NamasteDsa.jsx
@@ -4,3 +4,22 @@ function add(a,b){
 }
 
 add(5,10);
+
+
+
+/*
+# Q-1
+# Write a function that searches for a specific element in an array and returns its index. If the element is not found, return -1.
+**/ 
+
+let arr = [1,2,3,4,5,6,7,8,9];
+
+function checkIndex(arr, element){
+   for(let i = 0; i< arr.length; i++){
+    if(arr[i] === element){
+        return i;
+    } 
+    }
+    return -1;
+}
+
