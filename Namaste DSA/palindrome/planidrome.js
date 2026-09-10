dabc2f995c91a6b73d54b0fa784d47d484a0fa3b
Allan sam <allansam0905@gmail.com>
2026-09-10 22:27:40 +0530

    V2 the corrected version pf palindrome was added

--- Namaste DSA/palindrome/planidrome.js
+++ Namaste DSA/palindrome/planidrome.js
@@ -25,3 +25,36 @@ let x= 121
     }
 }
 
+
+// V2
+
+function isPalindrome() {
+
+
+    let x = 121
+    // Here by making a copy of x it will not change the original value of x and we can use it for comparison later
+    let Xcopy = x
+
+    // because negative numbers are not considered palindromes, we can return false if x is less than 0
+    if (x < 0) {
+        return false
+    }
+
+    let rev = 0;
+
+    while (x > 0) {
+        let rem = x % 10
+        rev = rev * 10 + rem;
+        // We need to use Math.floor to get the integer part of the division
+        x = Math.floor(x / 10)
+    }
+
+    if(rev === Xcopy) {
+        return true
+    } else {
+        return false
+    }
+
+    // return rev === Xcopy
+
+}
