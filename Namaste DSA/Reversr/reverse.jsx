13b2605c9dbeedc4191ed7e596747fcd1cf3a0bc
Allan sam <allansam0905@gmail.com>
2026-09-18 14:03:08 +0530

    V3 with solution added

--- Namaste DSA/Reversr/reverse.jsx
+++ Namaste DSA/Reversr/reverse.jsx
@@ -50,3 +50,22 @@ function rev(n){
         return (nCopy < 0 ) ? -rev : rev
     }
 }
+
+
+// V3
+
+
+var reverse = function(x) {
+  let xCopy = x;
+  x = Math.abs(x);
+  let rev = 0;
+  while (x > 0) {
+    let last = x % 10;
+    rev = rev * 10 + last;
+    x = Math.floor(x / 10);
+  }
+  if (rev > 2**31 - 1) return 0;
+  return xCopy < 0 ? -rev : rev;
+};
+
+console.log(reverse(123)); // 321
