3c87afd7a026cf52fe500fadaada7874c7798dd3
Allan sam <allansam0905@gmail.com>
2026-09-18 14:02:48 +0530

    v2 solution added

--- Namaste DSA/Reversr/reverse.jsx
+++ Namaste DSA/Reversr/reverse.jsx
@@ -26,3 +26,27 @@ function checkReverse(n){
 //Write a function reverse(x) that takes a 32-bit signed integer and returns its digits reversed. If the reversed value overflows the 32-bit signed integer range [-2^31 , 2^31-1], return 0.
 
 
+
+function rev(n){
+
+    let nCopy = n
+    // Here we handle a case
+    n = Math.abs(n)
+
+    let rev = 0
+
+    while(n > 0){
+        let rem = n %10
+        rev = rev * 10 + rem
+        // Here we handle a case
+        n = Math.floor(n /10)
+    
+         // Here we handle a case
+
+         let limit  = Math.pow(2,31)
+        //  let limit = 2**31 - denotes 2 power 31
+         if(rev <-limit || rev>limit) return 0
+
+        return (nCopy < 0 ) ? -rev : rev
+    }
+}
